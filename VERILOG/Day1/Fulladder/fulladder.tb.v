dc6dc761522052a89fb9e9233020d89e62e61262
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-02-22 20:43:16 +0530

    Create fulladder.tb.v

---
+++ VERILOG/Day1/Fulladder/fulladder.tb.v
@@ -0,0 +1,26 @@
+// Code your testbench here
+// or browse Examples
+module fd_tb;
+  reg A, B , Cin;
+  wire So, Co;
+  fd fulladder(.A(A), .B(B), .Cin(Cin), .So(So), .Co(Co));
+  initial begin
+    $dumpfile("fd.vcd");
+    $dumpvars(1, fd_tb);  
+  end
+
+  initial begin
+    #10 A = 1'b0; B = 1'b0; Cin = 1'b0;
+    #10 A = 1'b0; B = 1'b0; Cin = 1'b1;
+    #10 A = 1'b0; B = 1'b1; Cin = 1'b0;
+    #10 A = 1'b0; B = 1'b1; Cin = 1'b1;
+    #10 A = 1'b1; B = 1'b0; Cin = 1'b0;
+    #10 A = 1'b1; B = 1'b0; Cin = 1'b1;
+    #10 A = 1'b1; B = 1'b1; Cin = 1'b0;
+    #10 A = 1'b1; B = 1'b1; Cin = 1'b1;
+    #10
+    $finish;
+  end
+  always @(A,B,Cin)
+    $display("time=%0t \tINPUT VALUES: \t A=%b B=%b Cin=%b\t output value So = %b \t Co=%b", $time, A, B, Cin, So, Co);
+endmodule
