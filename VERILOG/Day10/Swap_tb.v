6fb9fd69f7c96d65ac5d6f3774ad5b948547fe78
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-03-03 22:38:16 +0530

    Create Swap_tb.v

---
+++ VERILOG/Day10/Swap_tb.v
@@ -0,0 +1,35 @@
+// Code your testbench here
+// or browse Examples
+module swap_tb;
+
+  reg a, b;
+
+  wire swapped_a, swapped_b;  
+ 
+  swap uut(a, b, swapped_a, swapped_b);
+  
+  initial begin
+    
+    a = 1;
+    b = 0;
+    #10
+    
+    a = 0;
+    b = 1;
+    #10
+    
+    a = 1;
+    b = 1;
+    #10
+    
+    a = 0;
+    b = 0;
+    #10
+    
+    $finish;
+  end
+  
+  always@(*)
+  $monitor("a=%d b=%d swapped_a=%d swapped_b=d",a,b,swapped_a,swapped_b);
+  
+endmodule
