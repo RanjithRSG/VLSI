b610751c7ef02b4b9086b6598ef91e5d8d01d137
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-03-03 11:38:46 +0530

    Update PrimeNumber.v

--- VERILOG/Day9/PrimeNumber.v
+++ VERILOG/Day9/PrimeNumber.v
@@ -1,22 +1,30 @@
 // Code your design here
 module prime_checker(
-    input wire [31:0] N,
+    input wire [32:0] N,
     output reg prime
 );
 
-reg [31:0] i;
-
 always @(*) begin
+    integer i;
     prime = 1;
-    if (N <= 1) begin
+  
+    if (N <= 1)
         prime = 0;
-    end
-    else begin
-        for (i = 2; i <= N/2; i = i + 1) begin
-            if (N % i == 0) 
-                prime = 0; 
+  
+    else if (N <= 3)
+        prime = 1;
+  
+    else if((N % 2 == 0) || (N % 3 == 0))
+        prime = 0;
+  
+    else
+        for (i = 5; i * i <= N; i = i + 1) begin
+            if (N % i == 0) begin
+                prime = 0;
+                break;
+            end
         end
-    end
 end
 
 endmodule
+
