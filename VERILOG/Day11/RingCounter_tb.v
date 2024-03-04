f3e17b1e876d2dc509b07a805e1fbf588f8eec2c
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-03-04 22:47:30 +0530

    Create RingCounter_tb.v

---
+++ VERILOG/Day11/RingCounter_tb.v
@@ -0,0 +1,36 @@
+// Code your testbench here
+// or browse Examples
+module ring_ctr_tb ; 
+  
+  parameter WIDTH = 4;  
+    
+  reg clk;  
+  reg rstn;  
+  wire [WIDTH-1:0] out;  
+    
+  ring_ctr  u0 (.clk (clk),  
+                .rstn (rstn),  
+                .out (out)
+               );  
+    
+  initial begin
+    
+    $dumpfile("ring_ctr.vcd");
+    $dumpvars(1);
+  
+  end
+  
+  always #10 clk = ~clk;  
+    
+  initial begin  
+    {clk, rstn} <= 0;  
+  
+    $monitor ("T=%0t out=%b", $time, out);  
+    repeat (2) @(posedge clk);  
+    rstn <= 1;  
+    repeat (15) @(posedge clk);  
+    $finish;  
+  
+  end  
+
+endmodule 
