4655e8a5411130e4636911082ce5d8c7c1bbd7da
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-06-29 13:17:00 +0530

    Create Integer.sv

---
+++ SYSTEM VERILOG/DATA TYPES/4-STATE/Integer.sv
@@ -0,0 +1,13 @@
+module integer_top;
+  
+  integer value;
+  
+  initial begin
+    
+    $display("default integer value = %0d",value);
+    value = 32'h6ab9cdef;
+    $display("value of integer is %0d",value);
+    
+  end
+  
+endmodule
