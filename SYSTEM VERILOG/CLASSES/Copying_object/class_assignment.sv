af0ea8fabb8ac38f50bc5ff0ae6991cb8e2647c0
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-07-01 14:28:25 +0530

    Create class_assignment.sv

---
+++ SYSTEM VERILOG/CLASSES/Copying_object/class_assignment.sv
@@ -0,0 +1,38 @@
+// Code your testbench here
+// or browse Examples
+class copy;
+  string name;
+  int id;
+  
+  function new();
+    name = "vignesh";
+    id = 35;
+  endfunction
+  
+  function void display();
+        $display("Name = %s  Id = %0d",name, id);
+  endfunction
+  
+endclass
+
+module top;
+  
+  copy c1;
+  copy c2;
+  
+  initial 
+    begin
+      c1 = new();
+      $display("before name changing");
+      c1.display();
+      c2 = c1;
+      $display("before id changing");
+      c2.display();
+      c2.name = "karthi";
+      c2.id = 05;
+      $display("after name changing");
+      c1.display();
+      $display("after id changing");
+      c1.display();
+    end
+endmodule
