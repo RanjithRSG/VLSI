6681770687a80ad81292981b7c990399d63c2368
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-07-01 12:26:27 +0530

    Create Instance.sv

---
+++ SYSTEM VERILOG/CLASSES/Constants/Instance.sv
@@ -0,0 +1,31 @@
+// Code your testbench here
+// or browse Examples
+class data;
+  const int a;
+  string b;
+  
+  function new();
+    a = 5;
+    b = "ANBELLA";
+  endfunction:new
+  
+  function void display();
+    $display("a=%0d,b=%0d",a,b);
+  endfunction:display
+
+endclass
+
+module instance_class;
+  
+  data t1;
+  
+  initial 
+    begin
+      t1=new();
+      t1.b="bjt";
+      $display("INSTANCE VARIABLE");
+      t1.display();
+      $display("Example");
+    end
+
+endmodule
