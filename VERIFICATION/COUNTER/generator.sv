c69ee885abe535b98e8ef63cff3a3f3642379977
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-07-06 15:45:30 +0530

    Create generator.sv

---
+++ VERIFICATION/COUNTER/generator.sv
@@ -0,0 +1,18 @@
+class generator;
+  transaction pkt;
+  mailbox mail;
+
+  function new(mailbox mail);
+    this.mail = mail;
+  endfunction
+
+  task gene;
+    repeat (8) begin
+      pkt = new();
+      pkt.randomize();
+      mail.put(pkt);
+      $display("#####Done#####");
+      #10;
+    end
+  endtask
+endclass
