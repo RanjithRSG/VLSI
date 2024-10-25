e32c8716c77a73123d56fd786e6b3adc62a06d64
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-10-25 19:26:21 +0530

    Create test.sv

---
+++ VERIFICATION/HALF_ADDER/test.sv
@@ -0,0 +1,25 @@
+`include "environment.sv"
+
+class hatest;
+  environment env;
+  mailbox mail;
+  virtual operation vintf;
+  
+  function new(virtual operation vintf, mailbox mail);
+    this.vintf=vintf;
+    this.mail=mail;
+  endfunction
+  
+  task memory;
+    mail=new();
+    env=new(vintf,mail);
+  endtask
+  
+  task run;
+    fork
+    env.memory;
+    env.run;
+    join
+  endtask
+endclass
+  
