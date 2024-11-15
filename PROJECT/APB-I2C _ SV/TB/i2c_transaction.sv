f2d9a79ce6d9281c78b8799243f7c6d8bfd471f3
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-11-15 17:41:47 +0530

    Create i2c_transaction.sv

---
+++ PROJECT/APB-I2C _ SV/TB/i2c_transaction.sv
@@ -0,0 +1,11 @@
+class i2c_transaction;
+  
+  logic scl_pad_i;
+  logic scl_pad_o;
+  logic scl_padoen_o;
+  logic sda_pad_i;
+  logic sda_pad_o;
+  logic sda_padeon_o;
+  logic interrupt_o;
+  
+endclass
