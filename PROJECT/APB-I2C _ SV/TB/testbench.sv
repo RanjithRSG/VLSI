2db8832d0f09fde2b569d5a5232ade429b0399d2
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2024-11-15 17:39:49 +0530

    Update testbench.sv

--- PROJECT/APB-I2C _ SV/TB/testbench.sv
+++ PROJECT/APB-I2C _ SV/TB/testbench.sv
@@ -1,1 +1,50 @@
+// Code your testbench here
+// or browse Examples
+`include "apb_interface.sv"
+`include "test.sv"
 
+module tb;
+  
+  apb_interface intf();
+  test_top test;
+  mailbox mail;
+  mailbox mbox;
+  
+  apb_i2c dut(.HCLK(intf.HCLK),
+              .HRESETn(intf.HRESETn),
+              .PADDR(intf.PADDR),
+              .PWDATA(intf.PWDATA),
+              .PWRITE(intf.PWRITE),
+              .PSEL(intf.PSEL),
+              .PENABLE(intf.PENABLE),
+              .PRDATA(intf.PRDATA),
+              .PREADY(intf.PREADY),
+              .PSLVERR(intf.PSLVERR),
+              .interrupt_o(intf.interrupt_o)
+             );
+  
+  
+  always #5 intf.HCLK = ~intf.HCLK;
+
+  initial begin
+    mail = new();
+    test = new(mail, mbox, intf);
+    intf.HCLK = 0;
+    intf.HRESETn = 0;
+    #20
+    intf.HRESETn = 1;
+    test.run();
+    #2000
+    $finish;
+  end
+  
+  initial begin 
+    fork
+      begin
+        test.memory;
+        test.run;
+      end
+    join
+  end
+
+endmodule
