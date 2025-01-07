1632378bb3984736c15cdd8dedaf2672d15a2f62
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2025-01-07 09:02:39 +0530

    Update interfcae.sv

--- DEBUG-ASSIGNMENT/SV/interfcae.sv
+++ DEBUG-ASSIGNMENT/SV/interfcae.sv
@@ -3,5 +3,8 @@ interface alu_if(input logic clk);
     logic [7:0] a, b;
     logic [1:0] op;
     logic [7:0] result;
+
+//     modport drv (output a, b, op, input result);
+//     modport mon (input a, b, op, result);
   
 endinterface
