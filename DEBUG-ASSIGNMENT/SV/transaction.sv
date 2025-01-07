e751c917acb39095d12e29711a255b6d11203fe2
RanjithRSG <149086310+RanjithRSG@users.noreply.github.com>
2025-01-07 09:03:01 +0530

    Update transaction.sv

--- DEBUG-ASSIGNMENT/SV/transaction.sv
+++ DEBUG-ASSIGNMENT/SV/transaction.sv
@@ -3,5 +3,13 @@ class alu_transaction;
     rand bit [7:0] a, b;
     rand bit [1:0] op;
     bit [7:0] expected_result;
+
+//     function new();
+//         super.new();
+//     endfunction
+
+//     function void print();
+//         $display("ALU Transaction: a=%0h, b=%0h, op=%0h, expected=%0h", a, b, op, expected_result);
+//     endfunction
   
 endclass
